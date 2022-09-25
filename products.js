912fb0facaf56530d69c1fe301ee7e4d4f8c2d02
MahdiAsgary <mdi.asgary@gmail.com>
2022-09-25 09:02:02 +0330

    add products list

---
+++ products.js
@@ -0,0 +1,52 @@
+export const productsData = [
+
+    {
+        id : 1 ,
+        title : "msi -GETFORCE LP 1200 ",
+        price : 89 ,
+        imageUrl : "./images/product-1.jpg"
+    } ,
+    {
+        id : 2 ,
+        title : "zotac -GETFORCE RTX 3060 ",
+        price : 149 ,
+        imageUrl : "./images/product-2.jpg" 
+    } ,
+    {
+
+        id : 3 ,
+        title : "gigabyte -GETFORCE GT 1030 ",
+        price : 87 ,
+        imageUrl : "./images/product-3.jpg"
+    } ,
+    {
+        id : 4 ,
+        title : "msi -GETFORCE GT 1030 ",
+        price : 99 ,
+        imageUrl : "./images/product-4.jpg"
+    } ,
+    {
+        id : 5 ,
+        title : "asus -GETFORCE LP 710 ",
+        price : 129 ,
+        imageUrl : "./images/product-5.jpg"
+    } ,
+    {
+        id : 6 ,
+        title : "asus -GETFORCE LP 1050 ",
+        price : 89 ,
+        imageUrl : "./images/product-6.jpg"
+    } , 
+    {
+        id : 7 ,
+        title : "zotac -GETFORCE GT 710 ",
+        price : 66 ,
+        imageUrl : "./images/product-7.jpg"
+    },
+    {
+        id : 8 ,
+        title : "zotac -GETFORCE GT 730 ",
+        price : 119 ,
+        imageUrl : "./images/product-8.jpg"
+    }
+]
